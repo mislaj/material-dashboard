793da4b58fb6db7501d318b3bad1626912dc747a
Mislaj NU <mislajnu@Mislajs-MacBook-Air.local>
2025-04-14 19:20:05 +0400

    updates

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./breadcrumb.css";
+import "./Breadcrumb.css";
 
 const Breadcrumb = () => {
   return (
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaUser, FaCog, FaBell } from "react-icons/fa";
-import "./topbar.css";
+import "./Topbar.css";
 
 const Topbar = () => {
   return (
